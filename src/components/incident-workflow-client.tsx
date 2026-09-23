d9fcb95b8debb7ed38738c4d2238bc3d88f16a49
qarica <codepm.ttv@gmail.com>
2026-09-23 09:14:34 +0700

    Keep client directive before dictation import

--- src/components/incident-workflow-client.tsx
+++ src/components/incident-workflow-client.tsx
@@ -1,4 +1,4 @@
-import { DictationTextarea } from "@/components/dictation-textarea";\n"use client";
+"use client";\nimport { DictationTextarea } from "@/components/dictation-textarea";
 import {FormEvent,useMemo,useState} from "react";import {useRouter} from "next/navigation";
 import {matchNC3} from "@/lib/nc3-classifier";
 import {IncidentContributingFactorsClient} from "@/components/incident-contributing-factors-client";
